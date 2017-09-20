2527537e54629056f3509410c2a6d71be22f63d4
Chris Ulanowicz <src3collector@gmail.com>
2017-09-20 10:34:35 -0500

    added a second version of Fibonacci

--- arrays/Fibonacci.cs
+++ arrays/Fibonacci.cs
@@ -5,6 +5,7 @@ using System;
 public class Program
 {
 
+	// this method builds the array containing the sequence
 	public static int Fibonacci(int idx)
 	{
 		int[] FibSequence = new int[idx+1];
@@ -26,6 +27,25 @@ public class Program
 		return FibSequence[idx];
 	}
 
+	// this method only uses the previous 2 values without taking up space with an array
+	public static int Fibonacci2(int idx)
+	{
+		if(idx == 0 || idx == 1)
+		{
+			return idx;
+		}
+		int Fib1 = 0;
+		int Fib2 = 1;
+		int Fib3;
+		for(int i=2;i<=idx;i++)
+		{
+			Fib3 = Fib1 + Fib2;
+			Fib1 = Fib2;
+			Fib2 = Fib3;
+		}
+		return Fib2;
+	}
+
 	public static void Main(string[] args)
 	{
 		// test cases  ex. fib sequence [0,1,1,2,3,5,8,13,21,34]
@@ -34,6 +54,12 @@ public class Program
 		Console.WriteLine(Fibonacci(2));
 		Console.WriteLine(Fibonacci(5));
 		Console.WriteLine(Fibonacci(8));
+
+		Console.WriteLine(Fibonacci2(0));
+		Console.WriteLine(Fibonacci2(1));
+		Console.WriteLine(Fibonacci2(2));
+		Console.WriteLine(Fibonacci2(5));
+		Console.WriteLine(Fibonacci2(8));
 	}
 
 }
