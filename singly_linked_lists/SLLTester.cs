6889cbbac8b9cdda16b20872fa8dd4d64c1a3de7
Chris Ulanowicz <src3collector@gmail.com>
2017-10-16 09:57:31 -0500

    cleaning up test file in singly linked list

--- singly_linked_lists/SLLTester.cs
+++ singly_linked_lists/SLLTester.cs
@@ -10,47 +10,60 @@ namespace SLL
 		{
 			SinglyLinkedList TestSLL = new SinglyLinkedList();
 			Console.WriteLine(TestSLL.SLLToString()); // Head->null
-			Console.WriteLine(TestSLL.Count()); // 0
+
+			// SLLAddBack tests
+			TestSLL.AddBack(5);
+			TestSLL.AddBack(2);
+			TestSLL.AddBack(4);
+			Console.WriteLine(TestSLL.SLLToString()); // Head->5->2->4->null
+
+			// SLLAddFront tests
+			TestSLL = new SinglyLinkedList(); // reset list to empty
+			TestSLL.AddFront(5);
+			TestSLL.AddFront(2);
+			TestSLL.AddFront(4);
+			Console.WriteLine(TestSLL.SLLToString()); // Head->4->2->5->null
+
+			// Console.WriteLine(TestSLL.Count()); // 0
 			// Console.WriteLine(TestSLL.Front()); // null
 			// Console.WriteLine(TestSLL.Back()); // null
 			// Console.WriteLine(TestSLL.IsEmpty()); // True
 			// Console.WriteLine(TestSLL.Contains(5)); // False
-			var results1 = TestSLL.MinMaxAverage();
-			foreach(var result in results1)
-			{
-				Console.WriteLine(result);
-			}
-			var Removed1 = TestSLL.RemoveBack();
-			Console.WriteLine(Removed1); // null
-			var RemovedFront1 = TestSLL.RemoveFront();
-			Console.WriteLine(RemovedFront1);
-			Console.WriteLine(TestSLL.SLLToString());  // Head->null
-			TestSLL.AddBack(5);
-			Console.WriteLine(TestSLL.SLLToString()); // Head->5->null
-			TestSLL.AddFront(8);
-			Console.WriteLine(TestSLL.SLLToString()); // Head->8->5->null
-			TestSLL.AppendValue(7,8);
-			Console.WriteLine(TestSLL.SLLToString()); // Head->8->7->5->null
-			TestSLL.PrependValue(6,5);
-			Console.WriteLine(TestSLL.SLLToString()); // Head->8->7->6->5->null
+			// var results1 = TestSLL.MinMaxAverage();
+			// foreach(var result in results1)
+			// {
+			// 	Console.WriteLine(result);
+			// }
+			// var Removed1 = TestSLL.RemoveBack();
+			// Console.WriteLine(Removed1); // null
+			// var RemovedFront1 = TestSLL.RemoveFront();
+			// Console.WriteLine(RemovedFront1);
+			// Console.WriteLine(TestSLL.SLLToString());  // Head->null
+			// Console.WriteLine(TestSLL.SLLToString()); // Head->5->null
+			// TestSLL.AddFront(8);
+			// Console.WriteLine(TestSLL.SLLToString()); // Head->8->5->null
+			// TestSLL.AppendValue(7,8);
+			// Console.WriteLine(TestSLL.SLLToString()); // Head->8->7->5->null
+			// TestSLL.PrependValue(6,5);
+			// Console.WriteLine(TestSLL.SLLToString()); // Head->8->7->6->5->null
 			// Console.WriteLine(TestSLL.Back()); // 5
 			// Console.WriteLine(TestSLL.Front()); // 8
 			// Console.WriteLine(TestSLL.Contains(5)); // True
 			// Console.WriteLine(TestSLL.Contains(2)); // False
 			// Console.WriteLine(TestSLL.IsEmpty()); // False
-			Console.WriteLine(TestSLL.Count()); // 4
-			Console.WriteLine(TestSLL.MaxToBack().SLLToString());
-			var results = TestSLL.MinMaxAverage();
-			foreach(var result in results)
-			{
-				Console.WriteLine(result);
-			}
-			var Removed = TestSLL.RemoveBack();
-			Console.WriteLine(Removed.Value); // 8
-			Console.WriteLine(TestSLL.SLLToString());  // Head->7->6->5->null
-			var RemovedFront2 = TestSLL.RemoveFront();
-			Console.WriteLine(RemovedFront2.Value); // 7
-			Console.WriteLine(TestSLL.SLLToString()); // Head->6->5->null
+			// Console.WriteLine(TestSLL.Count()); // 4
+			// Console.WriteLine(TestSLL.MaxToBack().SLLToString());
+			// var results = TestSLL.MinMaxAverage();
+			// foreach(var result in results)
+			// {
+			// 	Console.WriteLine(result);
+			// }
+			// var Removed = TestSLL.RemoveBack();
+			// Console.WriteLine(Removed.Value); // 8
+			// Console.WriteLine(TestSLL.SLLToString());  // Head->7->6->5->null
+			// var RemovedFront2 = TestSLL.RemoveFront();
+			// Console.WriteLine(RemovedFront2.Value); // 7
+			// Console.WriteLine(TestSLL.SLLToString()); // Head->6->5->null
 		}
 
 	}
